2840f83d8d26034f1c5c92b57361beac6b03912d
iChris <christianjoseph473@gmail.com>
2022-02-24 01:58:11 +0800

    changing to only next files

--- router/private.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router()
-
-const privateController = require('../controller/private')
-const {protect} = require('../middleware/auth')
-
-router.get('/', protect, privateController.getPrivateData)
-
-module.exports = router
