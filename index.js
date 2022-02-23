b55cfcb6fe73b113915667fbabe8a521ef7460d7
iChris <christianjoseph473@gmail.com>
2022-02-23 11:31:25 +0800

    restructured indexjs express file

--- index.js
+++ index.js
@@ -18,61 +18,61 @@ app.use("/api/private", private)
 
 const port = process.env.PORT || 3000
  
-app.use(express.static(path.join(__dirname, 'client', 'build')))
+app.use(express.static(path.join(__dirname, './client/build')))
 
 app.get('/', function(req, res) {
   res.sendFile(
-    path.join(__dirname, 'client', 'build', 'index.html'), (err => err && res.status(500).send(err))
+    path.join(__dirname, './client/build/index.html'), (err => err && res.status(500).send(err))
   )
 })
 app.get('/profile', function(req, res) {
   res.sendFile(
-    path.join(__dirname, 'client', 'build', 'profile.html'), (err => err && res.status(500).send(err))
+    path.join(__dirname, './client/build/profile.html'), (err => err && res.status(500).send(err))
   )
 })
 app.get('/admin', function(req, res) {
   res.sendFile(
-    path.join(__dirname, 'client', 'build', 'admin.html'), (err => err && res.status(500).send(err))
+    path.join(__dirname, './client/build/admin.html'), (err => err && res.status(500).send(err))
   )
 })
 app.get('/aboutus', function(req, res) {
   res.sendFile(
-    path.join(__dirname, 'client', 'build', 'aboutus.html'), (err => err && res.status(500).send(err))
+    path.join(__dirname, './client/build/aboutus.html'), (err => err && res.status(500).send(err))
   )
 })
 app.get('/editemail', function(req, res) {
   res.sendFile(
-    path.join(__dirname, 'client', 'build', 'editemail.html'), (err => err && res.status(500).send(err))
+    path.join(__dirname, './client/build/editemail.html'), (err => err && res.status(500).send(err))
   )
 })
 app.get('/editprofile', function(req, res) {
   res.sendFile(
-    path.join(__dirname, 'client', 'build', 'editprofile.html'), (err => err && res.status(500).send(err))
+    path.join(__dirname, './client/build/editprofile.html'), (err => err && res.status(500).send(err))
   )
 })
 app.get('/editpassword', function(req, res) {
   res.sendFile(
-    path.join(__dirname, 'client', 'build', 'editpassword.html'), (err => err && res.status(500).send(err))
+    path.join(__dirname, './client/build/editpassword.html'), (err => err && res.status(500).send(err))
   )
 })
 app.get('/forgotpassword', function(req, res) {
   res.sendFile(
-    path.join(__dirname, 'client', 'build', 'forgotpassword.html'), (err => err && res.status(500).send(err))
+    path.join(__dirname, './client/build/forgotpassword.html'), (err => err && res.status(500).send(err))
   )
 })
 app.get('/register', function(req, res) {
   res.sendFile(
-    path.join(__dirname, 'client', 'build', 'register.html'), (err => err && res.status(500).send(err))
+    path.join(__dirname, './client/build/register.html'), (err => err && res.status(500).send(err))
   )
 })
 app.get('/signin', function(req, res) {
   res.sendFile(
-    path.join(__dirname, 'client', 'build', 'signin.html'), (err => err && res.status(500).send(err))
+    path.join(__dirname, './client/build/signin.html'), (err => err && res.status(500).send(err))
   )
 })
 app.get('/resetpassword', function(req, res) {
   res.sendFile(
-    path.join(__dirname, 'client', 'build', 'resetpassword.html'), (err => err && res.status(500).send(err))
+    path.join(__dirname, './client/build/resetpassword.html'), (err => err && res.status(500).send(err))
   )
 })
 
